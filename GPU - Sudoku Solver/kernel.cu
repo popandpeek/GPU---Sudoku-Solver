e35f26ced2e9f5290c6e2cd2e90878f37802dbac
Calvin <calvinfei@live.com>
2019-03-19 08:30:33 -0700

    working add some more cases in main

--- GPU - Sudoku Solver/kernel.cu
+++ GPU - Sudoku Solver/kernel.cu
@@ -1,4 +1,3 @@
-
 #include "cuda_runtime.h"
 #include "device_launch_parameters.h"
 #include <stdio.h>
@@ -6,21 +5,51 @@
 #include <cuda_runtime.h>
 #include <driver_functions.h>
 
-#include <stdio.h>
-
-#include <stdio.h>
 #include <stdlib.h>
+#include <cmath>
+#include <vector>
+#include <cstring>
 #include <iostream>
-#include <set>
-#include <chrono>
-#include <string>
+#include <fstream>
 
-
+#define THREADS_PER_BLOCK 512
 #define BOARD_SIZE 81
 #define SUB_BOARD_SIZE 9
 #define SUB_BOARD_DIM 3
 
-#define THREAD_PER_BLOCK 1024
+void print_board_1d(int *board) {
+
+	char* border = new char[26]{ "|-------+-------+-------|" };
+
+	std::cout << border << std::endl;
+	int split = sqrt(9);
+	for (int i = 0; i < 9 * 9; i++) {
+		if (i % 9 == 0) {
+			std::cout << "| ";
+		}
+		else if (i % split == 0) {
+			std::cout << "| ";
+		}
+
+		int value = board[i];
+		if (value != 0) {
+			std::cout << value << " ";
+		}
+		else {
+			std::cout << ". ";
+		}
+
+		if (i % 9 == 9 - 1) {
+			std::cout << "|" << std::endl;
+
+			if (((i + 1) % (9 * 9 / split)) == 0) {
+				std::cout << border << std::endl;
+			}
+		}
+	}
+	std::cout << std::endl;
+}
+
 
 #pragma region Boards
 /*Boards*/
@@ -131,6 +160,97 @@ int* diabolical_test_answer = new int[81]{ 2, 9, 5, 7, 4, 3, 8, 6, 1,
 
 #pragma endregion
 
+// function to examine if there are conflicts or not if cell [row][col] is num
+__device__
+bool noConflicts(int matrix[BOARD_SIZE], int row, int col, int num) {
+	if (num <= 0 || num > SUB_BOARD_SIZE) return false;
+	for (int i = 0; i < SUB_BOARD_SIZE; i++) {
+		if (i == row)   continue;
+		if (matrix[i * SUB_BOARD_SIZE + col] == num) {
+			return false;
+		}
+	}
+
+	for (int j = 0; j < SUB_BOARD_SIZE; j++) {
+		if (j == col)   continue;
+		if (matrix[row * SUB_BOARD_SIZE + j] == num) {
+			return false;
+		}
+	}
+
+	for (int i = 0; i < SUB_BOARD_DIM; i++) {
+		for (int j = 0; j < SUB_BOARD_DIM; j++) {
+			int mat_row = (row / SUB_BOARD_DIM)*SUB_BOARD_DIM + i;
+			int mat_col = (col / SUB_BOARD_DIM)*SUB_BOARD_DIM + j;
+			if (mat_row == row && mat_col == col)   continue;
+			if (matrix[mat_row * SUB_BOARD_SIZE + mat_col] == num) {
+				return false;
+			}
+		}
+	}
+	return true;
+}
+
+__device__ bool row_check_dev(const int* _board, int _board_root, int _row, int _entry, int loc)
+{
+	for (int i = _row * _board_root; i < _row * _board_root + _board_root; i++) {
+		if (i != loc && _board[i] == _entry)
+		{
+			return false;
+		}
+	}
+
+	return true;
+}
+
+__device__ bool column_check_dev(const int* _board, int _board_root, int _col, int _entry, int loc)
+{
+	for (int i = _col; i < _board_root * _board_root - (_board_root - _col); i += _board_root) {
+		if (i != loc && _board[i] == _entry) {
+			return false;
+		}
+	}
+
+	return true;
+}
+
+__device__ bool grid_check_dev(const int* _board, int _board_root, int _start_row, int _start_col, int _entry, int loc)
+{
+	int sub_grid_x = _start_row / SUB_BOARD_DIM; // 0, 1, or 2
+	int sub_grid_y = _start_col / SUB_BOARD_DIM; // 0, 1, or 2
+	int grid_start = (sub_grid_x * SUB_BOARD_SIZE * SUB_BOARD_DIM) + (sub_grid_y * SUB_BOARD_DIM);
+	for (int i = 0; i < 3; i++) {
+		for (int j = 0; j < 3; j++) {
+			//		  start ind     rows of grid         col
+			int ind = grid_start + (i * SUB_BOARD_SIZE) + j;
+			if (ind != loc && _board[ind] == _entry) {
+				return false;
+			}
+		}
+	}
+
+	return true;
+}
+
+__device__ bool is_legal_entry_dev(const int* _board, int _board_root, int _row, int _col, int _entry, int loc)
+{
+	return row_check_dev(_board, _board_root, _row, _entry, loc) &&
+		column_check_dev(_board, _board_root, _col, _entry, loc) &&
+		grid_check_dev(_board, _board_root, _row, _col, _entry, loc);
+}
+
+// Returns whether or not it is valid to put a value in specified location for this board
+__device__ bool IsLegal(int *_board, int _loc, int _val)
+{
+	if (is_legal_entry_dev(_board, SUB_BOARD_SIZE, _loc / SUB_BOARD_DIM, _loc % SUB_BOARD_DIM, _val, _loc)) {
+		_board[_loc] = _val;
+		return true;
+	}
+
+	return false;
+}
+
+
 
 __device__ bool row_check_dev(const int* _board, int _board_root, int _row, int _entry, int loc, int _boardStart)
 {
@@ -193,7 +313,7 @@ __device__ bool IsLegal(int *_board, int _loc, int _val, int _boardStart)
 
 // Find next empty cell in passed in board
 __device__ int FindNextEmptyCell(int* board, int _boardStart) {
-	for (int i = 0; i < BOARD_SIZE; i++) 
+	for (int i = 0; i < BOARD_SIZE; i++)
 	{
 		if (board[i + _boardStart] == 0) {
 			return i;
@@ -228,7 +348,7 @@ __global__ void GenerateBoardsByCell(int *old_boards, int old_board_num, int *ne
 				// where to start appending for the new board
 				int new_board_offset = atomicAdd(new_board_num, 1); // increment amount of boards at the new depth
 
-				for (int j = 0; j < BOARD_SIZE; j++) 
+				for (int j = 0; j < BOARD_SIZE; j++)
 				{
 					int ind = (new_board_offset * BOARD_SIZE) + j;
 
@@ -242,130 +362,143 @@ __global__ void GenerateBoardsByCell(int *old_boards, int old_board_num, int *ne
 
 // Use DFS to solve specified board per thread
 __global__ void SolveBoard(int *boards, int total_boards, int* solution) {
-	int t_idx = blockDim.x * blockIdx.x + threadIdx.x;
+	int t_idx = blockIdx.x * blockDim.x + threadIdx.x;
 
-	// Each thread does DFS on 1 board
+
 	if (t_idx < total_boards) {
+
+		int empty_cells = 0;
+
+		int* empty_indices = (int*)malloc(sizeof(int) * BOARD_SIZE);
+		int* thread_board = (int*)malloc(sizeof(int) * BOARD_SIZE);
+
 		int board_start = t_idx * BOARD_SIZE;
-		int* thread_board = (int*)malloc(sizeof(int) * BOARD_SIZE); // local board for ease
+		for (int i = 0; i < BOARD_SIZE; i++) {
+			int ind = board_start + i;
 
-		int empty_cells = 0; // total empty cells in this board
-		int* empty_indices = (int*)malloc(sizeof(int) * BOARD_SIZE); // theoretical biggest size where entire board is empty
-
-		for (int i = 0; i < BOARD_SIZE; i++) { // read prev board into a sudoku sized local array
-
-			thread_board[i] = boards[board_start + i];
+			thread_board[i] = boards[ind];
 			if (thread_board[i] == 0) {
-				empty_indices[empty_cells] = i; // Store the indices where board is empty
+				empty_indices[empty_cells] = i;
 				empty_cells++;
 			}
 		}
 
-		int empty_cells_filled = 0;
-		while (empty_cells_filled < empty_cells) {
-			int next_cell = empty_indices[empty_cells_filled];
+		int filled_empty_cells = 0;
+		while (filled_empty_cells >= 0 && filled_empty_cells < empty_cells) {
 
-			thread_board[next_cell] += 1; // Try numbers 1 through 9 until it passes is IsLegal
+			int next_cell = empty_indices[filled_empty_cells];
+			int row = next_cell / SUB_BOARD_SIZE;
+			int col = next_cell % SUB_BOARD_SIZE;
 
-			// TODO: Needs to be configured with no malloc
-			if (true /*IsLegal(thread_board, next_cell, thread_board[next_cell])*/) {
-				empty_cells_filled++;
+			int val = thread_board[next_cell] + 1;
+			thread_board[next_cell]++;
+
+			if (noConflicts(thread_board, row, col, val)) { // IsLegal does not work here?
+				filled_empty_cells++;
 			}
-			else if (thread_board[next_cell] >= BOARD_SIZE) {
+			else if (thread_board[next_cell] >= SUB_BOARD_SIZE) {
 				thread_board[next_cell] = 0;
-				empty_cells_filled--;
+				filled_empty_cells--;
 			}
 		}
-		if (empty_cells_filled == empty_cells) { // all empty cells filled so we allocate solution and are done
-			for (int i = 0; i < BOARD_SIZE; i++) {
-				solution[i] = thread_board[i];
-			}
+		if (filled_empty_cells == empty_cells) {
+			memcpy(solution, thread_board, BOARD_SIZE * sizeof(int));
 		}
 	}
 }
 
+// Every additional depth guesses one cell with every possible potential
+void GenerateBoardsBFS(int* prev_boards, int* new_board_num, int* new_boards, int depth) {
 
+	// 1 because of the first board. This will then change iteration according to the permutations at each depth
+	int prev_board_num = 1;
 
-// The main solve function to be called
-void solve(int *board, int depth) {
+	for (int i = 0; i < depth; i++) {
+		int block_num = (prev_board_num + THREADS_PER_BLOCK - 1) / THREADS_PER_BLOCK;
+		cudaMemset(new_board_num, 0, sizeof(int));
+		GenerateBoardsByCell << <block_num, THREADS_PER_BLOCK >> > (prev_boards, prev_board_num, new_boards, new_board_num);
+		int* tmp = prev_boards;
+		prev_boards = new_boards;
+		new_boards = tmp;
+		cudaMemcpy(&prev_board_num, new_board_num, sizeof(int), cudaMemcpyDeviceToHost);
+	}
+}
 
+// The main solve function
+void solve_board(int * board, int depth) {
+
+	// Board that wil FIRST hold the old depth boards (first board)
+	int *old_boards;
+
+	// This needs to be a pointer since we want to increment in a device function
+	int *old_board_num;
+
+	// Clear storage until second iteration
+	int *new_boards;
+
+	// Solution
+	int *solution;
 	int h_solution[BOARD_SIZE];
 	memset(h_solution, 0, BOARD_SIZE * sizeof(int));
 
-	int *new_boards;
-	int *old_boards;
-	int *solution;
-	int *next_board_num;
-
+	// Theoretical upper bound of boards each cell of a level having all 9 potentials
 	const int memSize = 81 * pow(9, depth);
 
-	// allocate device memory and set everything to 0
-	cudaMalloc(&next_board_num, sizeof(int));
-	cudaMalloc(&solution, BOARD_SIZE * sizeof(int));
+	// alloc device memory
+	cudaMalloc(&old_board_num, sizeof(int));
 	cudaMalloc(&new_boards, memSize * sizeof(int));
 	cudaMalloc(&old_boards, memSize * sizeof(int));
+	cudaMalloc(&solution, BOARD_SIZE * sizeof(int));
 
-	cudaMemset(next_board_num, 0, sizeof(int));
-	cudaMemset(solution, 0, BOARD_SIZE * sizeof(int));
-	cudaMemset(new_boards, 0, memSize * sizeof(int));
+
+	cudaMemset(old_board_num, 0, sizeof(int));
 	cudaMemset(old_boards, 0, memSize * sizeof(int));
+	cudaMemset(new_boards, 0, memSize * sizeof(int));
+	cudaMemset(solution, 0, BOARD_SIZE * sizeof(int));
 
-
-	// BFS on GPU
-
-	// copy starting board to device memory
+	// Copy the starting board into our storage array 
 	cudaMemcpy(old_boards, board, BOARD_SIZE * sizeof(int), cudaMemcpyHostToDevice);
 
-	// 1 due to starting board
-	int old_board_num = 1;
-	for (int i = 0; i < depth; i++) {
+	// generates a set of boards with the first depth cells filled in
+	GenerateBoardsBFS(old_boards, old_board_num, new_boards, depth);
 
-		// Need 1 thread per old board
-		int num_blocks = (old_board_num + THREAD_PER_BLOCK - 1) / THREAD_PER_BLOCK;
+	// get the total number of boards back
+	int total_board_num = 1;
+	cudaMemcpy(&total_board_num, old_board_num, sizeof(int), cudaMemcpyDeviceToHost);
 
-		// need to set next board num to 0 before we start next depth
-		cudaMemset(next_board_num, 0, sizeof(int));
-
-		// we will read from old boards first and put into new boards
-		GenerateBoardsByCell << <num_blocks, THREAD_PER_BLOCK >> > (old_boards, old_board_num, new_boards, next_board_num);
-
-		// old and new boards are swapped in order for us to reuse the memory since we only care about the last depth
-		int* temp_board = old_boards;
-		old_boards = new_boards;
-		new_boards = temp_board;
-
-		// update the amount of boards in previous depth for next iteration
-		cudaMemcpy(&old_board_num, next_board_num, sizeof(int), cudaMemcpyDeviceToHost);
-	}
-
-
-	// DFS on GPU
-	// total amount of boards is just the boards in last depth
-	int total_board_num = old_board_num;
-	//cudaMemcpy(&total_board_num, next_board_num, sizeof(int), cudaMemcpyDeviceToHost);
-
-	int num_blocks = (total_board_num + THREAD_PER_BLOCK - 1) / THREAD_PER_BLOCK;
-
-	SolveBoard << <num_blocks, THREAD_PER_BLOCK >> > (old_boards, total_board_num, solution);
-
+	// Now we solve each board per thread on the GPU by DFS
+	int block_num = (total_board_num + THREADS_PER_BLOCK - 1) / THREADS_PER_BLOCK;
+	SolveBoard << <total_board_num, THREADS_PER_BLOCK >> > (old_boards, total_board_num, solution);
 	cudaDeviceSynchronize();
 
-	// copy the device solution to the host
 	cudaMemcpy(h_solution, solution, BOARD_SIZE * sizeof(int), cudaMemcpyDeviceToHost);
 
-	// Print board?
+	// Get the solution and print for correctness
+	print_board_1d(h_solution);
 
+
+	// free all used memory
 	cudaFree(new_boards);
 	cudaFree(&old_boards);
+	cudaFree(&old_board_num);
 	cudaFree(&solution);
-	cudaFree(&next_board_num);
-
-	return;
 }
 
-int main()
-{
-	// Solve medium sized board and go to depth 7 of BFS
-	//solve(test_board_medium, 7);
-	solve(test_board_medium, 2);
+
+int main(int argc, char* argv[]) {
+
+	int* board = new int[81]{		  
+		0, 0, 0, 0, 0, 0, 3, 0, 0,
+		8, 5, 2, 3, 0, 0, 0, 0, 1,
+		0, 9, 0, 2, 0, 0, 0, 0, 4,
+		9, 7, 4, 0, 0, 0, 0, 0, 0,
+		0, 1, 0, 0, 6, 0, 0, 0, 0,
+		0, 0, 0, 0, 4, 0, 0, 0, 0,
+		6, 0, 9, 0, 8, 0, 0, 3, 7,
+		3, 0, 0, 0, 0, 0, 0, 6, 0,
+		0, 2, 0, 0, 0, 5, 0, 0, 0 };
+
+	solve_board(board, 5);
+
+	return 0;
 }
